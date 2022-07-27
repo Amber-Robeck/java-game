f329af02ed20cc3876e479f6e848b4f00efb87c7
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-07-26 21:32:16 -0500

    Added jFrame window

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,9 +1,22 @@
 package main;
 
-public class Game {
+import javax.swing.JFrame;
+
+public class Game extends JFrame {
+	
+	public Game() {
+		
+		setSize(400, 400);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Start of the game");
+		
+		Game game = new Game();
 
 	}
 
